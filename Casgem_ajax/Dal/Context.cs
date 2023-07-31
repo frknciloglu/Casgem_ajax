d9d77fe4d3bd7ee3ae3cb7d24ea9648cdbe7365e
frknciloglu <59924846+frknciloglu@users.noreply.github.com>
2023-07-31 12:04:26 +0300

    Add project files.

---
+++ Casgem_ajax/Dal/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_ajax.Dal
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-PBKE4PO\\SQLEXPRESS; initial catalog=CasgemAjaxDb; integrated Security=true");
+        }
+        public DbSet<Customer> Customers { get; set; }  
+    }
+}
---
+++ Casgem_ajax/Dal/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Casgem_ajax.Dal
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City{ get;set; }
+    }
+}
